3b69854c4e43ab32246c8d0696d74679ef2f0f7d
Spillz <billygcasey@hotmail.com>
2018-04-09 16:08:09 -0500

    got started. Created pages. Need to work on formats and then the blog creation page

--- main.py
+++ main.py
@@ -3,47 +3,75 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:beproductive@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 
-class Task(db.Model):
+class Blog(db.Model):#needed that all to creat database and ability to enter 
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    completed = db.Column(db.Boolean)
+    blg_title = db.Column(db.String(120))
+    blg_body = db.Column(db.String(120)) #i dont know the max
 
-    def __init__(self, name):
-        self.name = name
-        self.completed = False
+    def __init__(self, blg_title, blg_body):
+        self.blg_title = blg_title
+        self.blg_body = blg_body
+
+
+#class User(db.Model):
+
+#    id = db.Column(db.Integer, primary_key=True)
+#    email = db.Column(db.String(120), unique=True)
+#    password = db.Column(db.String(120))
+
+#    def __init__(self, email, password):
+#        self.email = email
+#        self.password = password
+
+
+#@app.route('/login')
+#def login():
+#    return render_template('login.html')
+
+
+#@app.route('/register')
+#def register():
+#    return render_template('register.html')
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    #if request.method == 'POST':
+        #task_name = request.form['task']
+        #new_task = Task(task_name)
+        #db.session.add(new_task)
+        #db.session.commit()
 
-    if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Task(task_name)
-        db.session.add(new_task)
-        db.session.commit()
-
-    tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template('todos.html',title="Get It Done!", 
-        tasks=tasks, completed_tasks=completed_tasks)
+    #tasks = Task.query.filter_by(completed=False).all()
+    #completed_tasks = Task.query.filter_by(completed=True).all()
+    #return render_template('blog.html',title="Build a Blog", tasks=tasks, completed_tasks=completed_tasks)
+    blog = Blog.query.all()
+   
+    return render_template('homepage.html',title="Build a Blog", blog=blog)
 
 
-@app.route('/delete-task', methods=['POST'])
-def delete_task():
+@app.route('/blog')
+def register():
+    return render_template('blog.html')
 
-    task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
-    task.completed = True
-    db.session.add(task)
-    db.session.commit()
 
-    return redirect('/')
+
+#@app.route('/delete-task', methods=['POST'])
+#def delete_task():
+
+#    task_id = int(request.form['task-id'])
+#    task = Task.query.get(task_id)
+#    task.completed = True
+#    db.session.add(task)
+#    db.session.commit()
+
+#    return redirect('/')
 
 
 if __name__ == '__main__':
